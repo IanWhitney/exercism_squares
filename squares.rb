7c90271c0c3ad0feec3578dcbe183bcebc26789f
Ian Whitney <whit0694@umn.edu>
2015-01-10 12:03:11 -0600

    Extract Nums class
    To handle the behavior of creating an array from a range

--- squares.rb
+++ squares.rb
@@ -1,23 +1,35 @@
 class Squares
-  attr_accessor :number
+  attr_accessor :numbers
 
   def initialize(number)
-    self.number = number
+    self.numbers = Nums.new(number)
   end
 
   def square_of_sums
-    (1..number).to_a.inject(0) do |ret, n|
+    numbers.inject(0) do |ret, n|
       ret += n
     end ** 2
   end
 
   def sum_of_squares
-    (1..number).to_a.inject(0) do |ret, n|
+    numbers.inject(0) do |ret, n|
       ret += n**2
     end
   end
 
   def difference
     square_of_sums - sum_of_squares
+  end
+end
+
+class Nums
+  include Enumerable
+
+  def initialize(number)
+    @members = (1..number).to_a
+  end
+
+  def each &block
+    @members.each{|member| block.call(member)}
   end
 end
